1eef835796501c33b91acdbbf8686d51676a731d
Roberto Freire <rpfreire1@espe.edu.ec>
2025-02-05 20:06:54 -0500

    Implementing Error Decoder to handle Exceptions

---
+++ Microservices v/OrderService/src/main/java/com/rpfreire/OrderService/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.rpfreire.OrderService.config;
+
+import com.rpfreire.OrderService.external.decoder.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
+    }
+}
---
+++ Microservices v/OrderService/src/main/java/com/rpfreire/OrderService/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.rpfreire.OrderService.exception;
+
+public class CustomException extends RuntimeException {
+    private String errorCode;
+    private int status;
+    public CustomException(String message, String errorCode, int status) {
+        super(message);
+        this.errorCode = errorCode;
+        this.status  = status;
+    }
+}
